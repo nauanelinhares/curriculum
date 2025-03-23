05881da7ff51f8df66292ca32f65a80edf98f099
nauanelinhares <nauane.linhares@gmail.com>
2025-03-23 12:07:14 -0300

    chore: update Vite configuration and homepage URL to reflect new project structure

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,10 @@
 import { defineConfig } from 'vite'
 import tailwindcss from "@tailwindcss/vite";
-import react from '@vitejs/plugin-react-swc'
+
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/drawing-project/",
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-    rollupOptions: {
-      output: {
-        // Ensure proper content types for JavaScript modules
-        entryFileNames: "assets/[name]-[hash].js",
-        chunkFileNames: "assets/[name]-[hash].js",
-        assetFileNames: "assets/[name]-[hash].[ext]",
-      },
-    },
-  },
+  base: "/curriculum/",
 });
