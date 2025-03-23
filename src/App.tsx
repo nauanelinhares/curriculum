25a419be3ce020aaff2134b731ece55827b94df7
nauanelinhares <nauane.linhares@gmail.com>
2025-03-23 11:12:07 -0300

    feat: add profile

--- src/App.tsx
+++ src/App.tsx
@@ -6,94 +6,82 @@ import linkedin from './assets/linkedin.png'
 import gmail from './assets/gmail.png'
 import { Grid2 } from '@mui/material'
 import Experiences from './components/Experiences'
-import { ExperienceProps } from './components/Experiences/types'
-import { SocialMediaProps } from './types/social-media'
+import userIcon from "./assets/user-icon.png";
+import SideMenu from "./components/SideMenu";
 
-
-const experiences: ExperienceProps[] = [
-  {
-    company: 'Masterpiece',
-    title: 'Sculpture Workshop Instructor',
-    startDate: '2022',
-    endDate: '2023',
-    description: 'I taught sculpture workshops to children and adults. I also helped them to create their own sculptures.',
-    iconCompany: 'iconCompany 1',
-  },
-  {
-    company: 'Company 2',
-    title: 'Job Title 2',
-    startDate: '2022',
-    endDate: '2023',
-    description: 'Description 2',
-    iconCompany: 'iconCompany 2',
-  },
-  {
-    company: 'Company 3',
-    title: 'Job Title 3',
-    startDate: '2022',
-    endDate: '2023',
-    description: 'Description 3 awuhdauhwduhadhuawduhawhudawhud awhudawh udhuaw dhuawhud awuhd awhudwauh dawhud huawuhdawhu dawuhd uhawduhawd uhawuhdaw uhduhaw duhawhud awuhdawuh dahuw dhuawuhd awuhdahwu dhuaw dhuawhu dawhu duhawudh awhudhuaw duhaw duhawuhd awhudaw uhdauhw duhw',
-    iconCompany: 'iconCompany 3',
-  }
-]
-
-const socialMedia: SocialMediaProps[] = [
-  {
-    label: 'Linkedin',
-    url: 'https://www.linkedin.com/in/k%C3%BCbra-g%C3%B6nen-477169220/',
-    icon: 'iconLinkedin',
-  },
-  {
-    label: 'Instagram',
-    url: 'https://www.instagram.com/kbragnn/',
-    icon: 'iconInstagram',
-  },
-  {
-    label: 'Email',
-    url: 'mailto:kubragnn.0@gmail.com',
-    icon: 'iconEmail',
-  }
-]
+import {
+  education,
+  skills,
+  languages,
+  user,
+  socialMedia,
+  experiences,
+} from "./Users/kubra-gonen";
 
 function App() {
- 
   return (
-    <>
-      <div>
-      
-      </div>
-      <h1 className="bg-gradient-to-r from-gray-500 to-stone-950 bg-clip-text text-5xl font-extrabold text-transparent ...">
-      Kübra Gönen
-    </h1>
-    <div className="flex flex-row gap-4 justify-center items-center m-4">
-  {[...Array(socialMedia.length)].map((_, i) => (
-    <div key={i} className="w-12 h-12">
-      <img 
-        src={socialMedia[i].icon === 'iconLinkedin' ? linkedin : socialMedia[i].icon === 'iconInstagram' ? instagram : gmail} 
-        alt={socialMedia[i].label} 
-        onClick={() => window.open(socialMedia[i].url, '_blank')}
-        className="rounded-2xl object-cover hover:scale-120 transition-all duration-300 grayscale"
-      />
-    </div>
-  ))}
-</div>
-<Experiences  experiences={experiences}/>
-<Grid2 container spacing={2}>
-  <Grid2 size={4}>
-    <Drawing image={residentEvil} title="Lady Dimitrescu" />
-  </Grid2>
-  <Grid2 size={4}>
-    <Drawing image={residentEvil} title="Lady Dimitrescu" />
-  </Grid2>
-  <Grid2 size={4}>
-    <Drawing image={residentEvil} title="Lady Dimitrescu" />
-  </Grid2>
-</Grid2>
- 
+    <div className="flex">
+      <SideMenu education={education} skills={skills} languages={languages} />
 
-      
-    </>
-  )
+      {/* Main content */}
+      <div className="w-3/4 p-6">
+        <h1 className="bg-gradient-to-r from-gray-500 to-stone-950 bg-clip-text text-5xl font-extrabold text-transparent ...">
+          {user.name}
+        </h1>
+        <h2 className="text-gray-500 text-2xl">{user.role}</h2>
+
+        <div className="flex flex-row gap-4 justify-center items-center m-4">
+          {[...Array(socialMedia.length)].map((_, i) => (
+            <div key={i} className="w-12 h-12">
+              <img
+                src={
+                  socialMedia[i].icon === "iconLinkedin"
+                    ? linkedin
+                    : socialMedia[i].icon === "iconInstagram"
+                    ? instagram
+                    : gmail
+                }
+                alt={socialMedia[i].label}
+                onClick={() => window.open(socialMedia[i].url, "_blank")}
+                className="rounded-2xl object-cover hover:scale-120 transition-all duration-300 grayscale"
+              />
+            </div>
+          ))}
+        </div>
+        <hr className="my-4 h-[1.75px]" />
+
+        <div className="flex flex-row gap-4 mt-8 items-center">
+          <img src={userIcon} alt="user" className="w-8 h-8 filter grayscale" />
+          <span className="text-gray-500 text-2xl">Profile</span>
+        </div>
+
+        <hr className="my-4 bg-gray-500 h-[1.75px]" />
+
+        <div className="flex flex-col gap-2 text-justify">
+          <span className="text-gray-500 ">{user.description}</span>
+        </div>
+
+        <hr className="my-4 bg-gray-500 h-[1.75px]" />
+
+        <Experiences experiences={experiences} />
+
+        <hr className="my-4 bg-gray-500 h-[1.75px]" />
+
+        <div className="text-gray-500 text-4xl pb-4">Portfolio</div>
+        <Grid2 container spacing={2}>
+          <Grid2 size={4}>
+            <Drawing image={residentEvil} title="Lady Dimitrescu" />
+          </Grid2>
+          <Grid2 size={4}>
+            <Drawing image={residentEvil} title="Lady Dimitrescu" />
+          </Grid2>
+          <Grid2 size={4}>
+            <Drawing image={residentEvil} title="Lady Dimitrescu" />
+          </Grid2>
+        </Grid2>
+      </div>
+    </div>
+  );
 }
 
 export default App
