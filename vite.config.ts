a30b4d94fde6fd14d11aee84c54ed1e529ebeaab
nauanelinhares <nauane.linhares@gmail.com>
2025-03-23 11:40:11 -0300

    fix: update base path in Vite configuration for drawing project

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/curriculum/",
+  base: "/drawing-project/",
 });
